9d912415a57d5f0fa715cd09383d1c3e2ae966c5
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-10-03 10:14:22 +0800

    Initial unary calculate functionalities

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,18 +11,26 @@ import UIKit
 class ViewController: UIViewController {
     
     // MARK: - DATA
-    var isFinishedTypingNumber = true
-//    var currentInput: String = "" {
-//        didSet {
-//            displayLabel.text = currentInput
-//        }
-//    }
-//
+    private var isFinishedTypingNumber = true
+    
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
-//        currentInput = ""
         isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
     }
 
     @IBAction func numButtonPressed(_ sender: UIButton) {
@@ -34,7 +42,6 @@ class ViewController: UIViewController {
             } else {
                 displayLabel.text! += numVal
             }
-//            currentInput += numVal
         }
         
     }
